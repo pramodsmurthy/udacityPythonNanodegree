b9222db2b06399fbe659d0d91bc7c303ad337818
Pramod Krishnamurthy <pramodsk.87@gmail.com>
2019-08-02 12:02:15 -0400

    First C++ Commit Hello World C++

---
+++ C++/HelloWorld/src/HelloWorld.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : HelloWorld.cpp
+// Author      : Pramod Krishnamurthy
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "From C++ Program" << endl;
+	return 0;
+}
